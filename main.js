543a021a05b562f34c25625037b96761e14a72f9
saufts <l.szwaczka90@gmail.com>
2023-05-29 00:07:15 +0200

    Add event listener to get user data from the form

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-test 
+// first step, we need to atatch event listener (submit) to the form to get user data
+
+// attach event (click) listeners to each "game box"
+
+// next, initialize the game
+
+// next, we need to check which gamemode we're playing
+
+// we need to set win conditions
+
+// we need to determine current player
+
+// after each move, check win conditions and if not met, set other player as active
+
+// human vs human, next implement easy ai, next impossible ai
+
+const form = document.querySelector('#myForm');
+
+form.addEventListener('submit', (event) => {
+    // prevent page refresh
+    event.preventDefault();
+    // initialize user form data
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+    document.querySelector('.modal-wrapper').setAttribute('hidden', true);
+    console.log(data);
+})
